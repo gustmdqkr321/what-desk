83ee05a61bbfa6f8be7c256dd30282280bb0e421
Park_SH <mbc3940@kookmin.ac.kr>
2024-03-06 17:21:22 +0900

    클래스 개수 count

--- back/detect.py
+++ back/detect.py
@@ -8,7 +8,9 @@ def detect(image_path, result_folder, conf_threshold=0.25):
     # model = YOLO("yolov8s.pt")
     model = YOLO("yolov8x.pt")
     # Predict
-    result = model.predict(image_path, save=False, conf=conf_threshold, classes =[41,56,58,59,60,62,63,64,66,73,74,75])
+    target_classes = [41,56,58,59,60,62,63,64,66,73,74,75]
+
+    result = model.predict(image_path, save=False, conf=conf_threshold, classes = target_classes)
 
     # 이미지 저장 경로
     file_name = os.path.splitext(os.path.basename(image_path))[0]
@@ -20,6 +22,14 @@ def detect(image_path, result_folder, conf_threshold=0.25):
     plots = result[0].plot()
     cv2.imwrite(save_path, plots)
 
+    cls_to_idx = {x: i for i, x in enumerate(target_classes)}
+
+    result = model.predict(image_path, save=False, conf=0.25, classes=target_classes)
+    object_counter = [0] * len(target_classes)
+
+    for box in result[0].boxes:
+        object_counter[cls_to_idx[box.cls.item()]] += 1
+    # print(object_counter)
     return save_path
 
 if __name__ == "__main__":
@@ -34,6 +44,6 @@ if __name__ == "__main__":
     image_paths = '/Users/park_sh/Desktop/what-desk/back/image'
 
     for file_name in os.listdir(image_paths):
-        if file_name.endswith((".jpg", ".jpeg", ".png")):
+        if file_name.endswith((".jpg", ".jpeg", ".png", ".avif")):
             image_path = os.path.join(image_paths, file_name)
             result_image_path = detect(image_path, result_folder,0.1)
