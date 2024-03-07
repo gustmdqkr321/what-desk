5ba65f635a7cffe6cfaeb2b31ac7ead1854b1c12
Cho Hanul <135517709+3809271@users.noreply.github.com>
2024-03-07 18:14:58 +0900

    Update RegisterForm.tsx

--- signforms/src/app/sign-up/RegisterForm.tsx
+++ signforms/src/app/sign-up/RegisterForm.tsx
@@ -1,11 +1,12 @@
-//signforms/src/app/sign-up/RegisterForm.tsx
-
 // RegisterForm.tsx
 import { useState } from 'react';
 import handleSubmit from '../api/user/sign-up/route'; // handleSubmit.ts 파일을 가져옴
 
 const RegisterForm = () => {
-  const [username, setUsername] = useState<string>('');
+  const [user_id, setUserid] = useState<string>('');
+  const [name, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [image, setImage] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
   const [error, setError] = useState<string>('');
@@ -13,14 +14,14 @@ const RegisterForm = () => {
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // handleSubmit 함수 호출
-    await handleSubmit(username, password, confirmPassword, setError);
+    await handleSubmit(user_id, name, email, image, password, confirmPassword, setError);
   };
 
   return (
     <form onSubmit={onSubmit}>
       <div>
         <label>username:</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="text" value={user_id} onChange={(e) => setUserid(e.target.value)} />
       </div>
       <div>
         <label>비밀번호:</label>
@@ -30,8 +31,16 @@ const RegisterForm = () => {
         <label>비밀번호 확인:</label>
         <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
       </div>
+      <div>
+        <label>닉네임:</label>
+        <input type="string" value={name} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>이메일:</label>
+        <input type="string" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
       {error && <div>{error}</div>}
-      <button type="submit">회원가입</button>
+      <button type="submit">회원가입하기</button>
     </form>
   );
 };
